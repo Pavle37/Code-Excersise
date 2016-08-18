0246c45895bf012b2a91a35cc7a59ee24e6b51ef
Pavle37 <paja37@yahoo.com>
2016-08-18 18:19:55 +0200

    ListView in MainActivity added
    ListView with dummy data created and loaded.

--- CodingExcersise/app/src/main/java/pavle/com/codingexcersise/MainActivity.java
+++ CodingExcersise/app/src/main/java/pavle/com/codingexcersise/MainActivity.java
@@ -20,6 +20,14 @@ package pavle.com.codingexcersise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final List<DataObject> list = new ArrayList<>();
+
+        //adds 30 elements to list for test purposes
+        for(int i = 0; i <= 30; i++){
+            DataObject tmp = new DataObject();
+            tmp.setTitle(getResources().getString(R.string.app_name));
+            tmp.setDescription(getResources().getString(R.string.lorem_ipsum));
+            list.add(tmp);
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.list_item_main, R.id.text1, list) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text1 = (TextView) view.findViewById(R.id.text1);
+                TextView text2 = (TextView) view.findViewById(R.id.text2);
+
+                text1.setText(list.get(position).getTitle());
+                text2.setText(list.get(position).getDescription());
+                return view;
+            }
+        };
+
+        ListView lvItems = (ListView) findViewById(R.id.lvMain);
+        lvItems.setAdapter(adapter);
+
     }
 }
